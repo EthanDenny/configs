0a832eb7b5bba2137658bd12ba02cbbd51a51f38
Ethan Denny <ethandenny@protonmail.com>
2025-10-16 22:00:35 -0230

    Use stow

--- setup.fish
+++ setup.fish
@@ -1,24 +1,9 @@
 #!/usr/bin/env fish
 
-# Setup script to copy configuration files to their proper locations
-
-# Copy tmux configuration
-echo "Copying .tmux.conf to ~/.tmux.conf..."
-cp .tmux.conf ~/.tmux.conf
-
-# Create fish config directory if it doesn't exist
-echo "Creating ~/.config/fish directories if needed..."
+# setup config directories if they don't exist
 mkdir -p ~/.config/fish/functions
 mkdir -p ~/.config/fish/themes
 
-# Copy fish functions
-echo "Copying fish functions..."
-for f in fish/functions/*.fish
-    cp $f ~/.config/fish/functions/
-end
-
-# Copy fish themes
-echo "Copying fish themes..."
-cp fish/themes/"Dracula Official.theme" ~/.config/fish/themes/
-
-echo "All done."
+# Create symlinks with stow
+stow -R -t ~ fish
+stow -R -t ~ tmux
