b9a9639754122fd8baf733a7e20ce2b2945096ca
Ethan Denny <ethandenny@protonmail.com>
2025-10-22 09:03:23 -0230

    Kill prev session

--- fish/.config/fish/functions/start-tmux.fish
+++ fish/.config/fish/functions/start-tmux.fish
@@ -2,6 +2,7 @@
 # this isn't in the actual config because I might want to use tmux for other
 # things
 function start-tmux
+    tmux kill-session
     tmux new-session -d -s spellbook -n "yarn dev" -c work/spellbook
     tmux new-window -t spellbook -n "fish" -c work/spellbook
     tmux send-keys -t spellbook:0 'gt sync; yarn dev' C-m
